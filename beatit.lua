f801b5c4e49cf54086a9c0c864b06cd4b45f3138
Sam Schmalzried <sam.schmalzried@protonmail.com>
2019-05-22 13:18:56 -0500

    added supporting modules
    I created two modules to support my main functionality - one to split strings into tables that can be indexed, and one to generate a random phrase from the two dictionary files that I am using.  Unfortunately the "beatit" module isn't working properly and declaring that variables are null when they should have string values, so it is non-functional.

---
+++ beatit.lua
@@ -0,0 +1,47 @@
+local mysplit = require("split")
+local verb_file = "dicts/verbs.txt"
+local noun_file = "dicts/nouns.txt"
+
+verb_len = 0
+for _ in io.lines(verb_file) do
+	verb_len = verb_len + 1
+end
+
+noun_len = 0
+for _ in io.lines(noun_file) do
+	noun_len = noun_len + 1
+end
+
+vdefault = "beating "
+ndefault = "off"
+
+function get_line(filename, linenum)
+	local i = 1
+	for line in io.lines(filename) do
+		if (i == num) then
+			return line
+		end
+		i = i + 1
+	end
+	return nil
+end
+
+M = {}
+
+function randphrase()
+	print("in randphrase")
+
+	verb = "beating "
+	if (verb_len > 0) then
+		verb = get_line(verb_file, math.random(verb_len))
+	end
+	noun = "off" 
+	if (noun_len > 0) then
+		noun = get_line(noun_file, math.random(noun_len))
+	end
+
+	return verb..noun	
+end
+M.randphrase = randphrase
+
+return M
--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local discordia = require('discordia')
 local client = discordia.Client()
+local beatit = require('beatit')
 
 token_file = io.open("token.txt", "r")
 token = token_file:read()
@@ -8,7 +9,25 @@ io.close(token_file)
 client:run('Bot '..token)
 --Bot should be connected now
 
-
 client:on('ready', function()
 	print('Logged in!')
 end)
+
+local prefix = "./"
+
+client:on('messageCreate', function(message)
+	pre_start = string.find(message.content, prefix)
+	if (pre_start == nil or pre_start ~= 1)
+	then
+		print("got a message in "..message.channel.mentionString)
+		return
+	end
+
+	if (string.find(message.content, prefix.."beatit") == 1)
+	then
+		local rphrase = beatit.randphrase()
+		print("beating it with "..rphrase)
+		message.channel:send(rphrase)
+	end
+end)
+
