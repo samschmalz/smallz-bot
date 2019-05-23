09afd8d1c2d3c219da8a857f69622137634db657
Sam Schmalzried <sam.schmalzried@protonmail.com>
2019-05-23 11:32:42 -0500

    Fixed nil values from phrase generation
    I was using the wrong name for the line number variable in the get_line function, which is why it would reach the end of the loop and return nil. Also added a phrases dictionary so you get a full phrase from the bot and not just a random "jerk off" euphemism.

--- beatit.lua
+++ beatit.lua
@@ -1,6 +1,12 @@
 local mysplit = require("split")
+local phrase_file = "dicts/phrases.txt"
 local verb_file = "dicts/verbs.txt"
 local noun_file = "dicts/nouns.txt"
+
+phrase_len = 0
+for _ in io.lines(phrase_file) do
+	phrase_len = phrase_len + 1
+end
 
 verb_len = 0
 for _ in io.lines(verb_file) do
@@ -12,13 +18,14 @@ for _ in io.lines(noun_file) do
 	noun_len = noun_len + 1
 end
 
+pdefault = "I got caught "
 vdefault = "beating "
 ndefault = "off"
 
 function get_line(filename, linenum)
 	local i = 1
 	for line in io.lines(filename) do
-		if (i == num) then
+		if (i == linenum) then
 			return line
 		end
 		i = i + 1
@@ -29,18 +36,22 @@ end
 M = {}
 
 function randphrase()
-	print("in randphrase")
+	math.randomseed(os.time())
 
+	phrase = "I was caught "
+	if (phrase_len > 0) then
+		phrase = get_line(phrase_file, math.random(phrase_len))
+	end
 	verb = "beating "
 	if (verb_len > 0) then
-		verb = get_line(verb_file, math.random(verb_len))
+		verb = get_line(verb_file, math.random(verb_len)) 
 	end
 	noun = "off" 
 	if (noun_len > 0) then
 		noun = get_line(noun_file, math.random(noun_len))
 	end
 
-	return verb..noun	
+	return phrase..verb..noun	
 end
 M.randphrase = randphrase
 
--- main.lua
+++ main.lua
@@ -19,14 +19,12 @@ client:on('messageCreate', function(message)
 	pre_start = string.find(message.content, prefix)
 	if (pre_start == nil or pre_start ~= 1)
 	then
-		print("got a message in "..message.channel.mentionString)
 		return
 	end
 
 	if (string.find(message.content, prefix.."beatit") == 1)
 	then
 		local rphrase = beatit.randphrase()
-		print("beating it with "..rphrase)
 		message.channel:send(rphrase)
 	end
 end)
